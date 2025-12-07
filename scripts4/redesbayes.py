edc5e3d3bbffad1148afc5d1ab48ea3c4d9dfaa1
JuanDiegoGallego <juandiego.gallenico@gmail.com>
2025-12-07 20:43:28 +0100

    Ejercicio 1

--- scripts4/redesbayes.py
+++ scripts4/redesbayes.py
@@ -9,3 +9,26 @@ from pgmpy.factors.discrete import TabularCPD
 RESOURCES_PATH = Path(__file__).parent.parent / "resources4"
 
 reader = BIFReader(RESOURCES_PATH / "consumo.bif")
+model = reader.get_model()
+
+var = reader.get_variables()
+valores = reader.get_states()
+edges = reader.get_edges()
+cpds = reader.get_values()
+
+print(f"Variables en el modelo:\n {var} \n")
+print(f"Valores de la variables en el modelo:\n {valores} \n")
+print(f"Aristas en el modelo:\n {edges} \n")
+print(f"CPDs en el modelo:\n {cpds} \n")
+
+model_infer = VariableElimination(model)
+
+q = model_infer.query(variables=["Bono"], evidence={"Ventanas": "No", "Iluminacion": "No", "PlacasSolares": "No"})
+print(f"Resultado de la consulta B | V=No, I=No, PS=No:\n {q} \n")
+
+q = model_infer.query(variables=["Bono"], evidence={"Ventanas": "Si", "Iluminacion": "Si", "PlacasSolares": "No"})
+print(f"Resultado de la consulta B | V=Si, I=Si, PS=No:\n {q} \n")
+q = model_infer.query(variables=["Bono"], evidence={"Ventanas": "Si", "Iluminacion": "No", "PlacasSolares": "Si"})
+print(f"Resultado de la consulta B | V=Si, I=No, PS=Si:\n {q} \n")
+q = model_infer.query(variables=["Bono"], evidence={"Ventanas": "No", "Iluminacion": "Si", "PlacasSolares": "Si"})
+print(f"Resultado de la consulta B | V=No, I=Si, PS=Si:\n {q} \n")
